370057bdcd58c1aeaa901102f65e84f2c7046d23
Severi Siltala <severi.siltala@gmail.com>
2025-10-07 13:41:27 +0300

    Pin change

---
+++ #vk1_weather_station.py
@@ -0,0 +1,19 @@
+#vk1_weather_station
+
+from machine import Pin
+from time import sleep
+from dht import DHT22
+
+sensor = DHT22(Pin(28))
+
+while True:
+    try:
+        sensor.measure
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+        print("Lämpötila: " {:.1f}%.format(temp))
+        print("Kosteus: " {:.1f}%.format(hum)) 
+    except OSError as e:
+        print("Sensor read error: ", e)
+    
+    sleep(2)
