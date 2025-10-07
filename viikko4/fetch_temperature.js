48113bb8573ce2c8551fb575f3543f30aefd7f56
Severi Siltala <severi.siltala@gmail.com>
2025-10-07 21:16:01 +0300

    dataa thingspeakistä

---
+++ viikko4/fetch_temperature.js
@@ -0,0 +1,17 @@
+const url = "https://api.thingspeak.com/channels/3086934/feeds.json?api_key=14OY40DG754S1AEI";
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    const feeds =data.feeds;
+
+    const temperatures = feeds.map(feed => ({
+        time: feed.created_at,
+        temp: parseFloat(feed.field1)
+    }));
+    document.getElementById("output").textContent = JSON.stringify(temperatures);
+})
+.catch(error => {
+    console.error("Error fetching data", error);
+    document.getElementById("output").textContent = "Error loading data";
+});
