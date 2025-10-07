83e60d3f83b50c682edf058d4782f371fa41bc10
Severi Siltala <severi.siltala@gmail.com>
2025-10-07 18:48:12 +0300

    virheiden korjaamista

--- viikko3/server.js
+++ viikko3/server.js
@@ -39,7 +39,7 @@ app.post('/api/users', (req, res) => {
     const{ name, email } = req.body;
     db.run(`INSERT INTO users (name, email) VALUES (?, ?)`, [name, email], function(err){
         if (err) return res.status(400).json({error: error.message})
-        res.status(201).json({id this.lastID, name, email})
+        res.status(201).json({id: this.lastID, name, email})
     })
     
 });
